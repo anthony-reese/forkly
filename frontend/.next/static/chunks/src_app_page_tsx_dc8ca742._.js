9ebb00beca23a43bd58cc1910448f7c93ee30462
anthony-reese <anthony-reese@users.noreply.github.com>
2025-07-28 21:25:05 +0400

    Remove large files and add proper .gitignore for node_modules and .next

--- frontend/.next/static/chunks/src_app_page_tsx_dc8ca742._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f2f93bc6._.js",
-  "static/chunks/src_ace31835._.js"
-],
-    source: "dynamic"
-});
